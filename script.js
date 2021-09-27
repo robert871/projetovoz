02b13db1eb354b7d088701e215b153cb34602d65
Roberto Lopes da Costa <71911175+robert871@users.noreply.github.com>
2021-09-27 19:15:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+ 
+function abrir(){
+   var el3=window.document.getElementById(`lista`)
+   var el2=window.document.getElementById(`close`)
+   var el4=window.document.getElementById(`menu`)
+   el3.classList.replace("ul","ul2")
+   el4.classList.replace("menu","menu2")
+   el2.classList.replace("but","but2")
+
+}
+function list(){
+   var el3=window.document.getElementById(`lista`)
+   var el2=window.document.getElementById(`close`)
+   var el4=window.document.getElementById(`menu`)
+   el3.classList.replace("ul2","ul")
+   el4.classList.replace("menu2","menu")
+   el2.classList.replace("but2","but")
+
+}
+   
